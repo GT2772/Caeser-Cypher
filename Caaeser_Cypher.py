457dca1bb8bddd956003eac2b6c686543c6622ac
George Thomas <georgethomas19tu@gmail.com>
2023-02-12 16:00:54 +0530

    Update Caaeser_Cypher.py

--- Caaeser_Cypher.py
+++ Caaeser_Cypher.py
@@ -1,5 +1,3 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
@@ -12,9 +10,8 @@ def cypher(text,shift):
   for char in text: 
     encoded_char = chr(ord(char) + shift)
     encoded_string += encoded_char
-  print(f"your {direction}d is {encoded_string}")
+  print(f"your {direction}d data is {encoded_string}")
 
 
 
 cypher(text,shift)
-
